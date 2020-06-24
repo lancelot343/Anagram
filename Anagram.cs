d92a2c3f2512ba9e05bca92072d262543a9a6359
lancelot343 <60350575+lancelot343@users.noreply.github.com>
2020-06-24 12:07:43 +0300

    Add files via upload

---
+++ Anagram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Anagram
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToLower();
+            string str2 = Console.ReadLine().ToLower();
+            char[] cr = new char[str.Length];
+            char[] cr2 = new char[str2.Length];
+            if (str.Length == str2.Length)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    cr[i] = str[i];
+                    cr2[i] = str2[i];
+                }
+
+                Array.Sort(cr);
+                Array.Sort(cr2);
+
+                bool isAnagram = false;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (cr[i] != cr2[i]) { }
+                    else { isAnagram = true; }
+                }
+                if (isAnagram)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Different length!");
+            }
+        }
+    }
+}
